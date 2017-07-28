74363077b03fda1f702ac23980757df817f9642c
jianghua huang <qwe7002@hotmail.com>
2017-07-28 18:04:26 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [ $(basename `pwd`) != "templates" ];then
+    echo "[Error] Please do this in the templates directory!"
+    exit
+fi
+if [ ! -d "casper" ]; then
+    git clone https://github.com/SilverBlogTeam/casper.git
+    cd casper
+fi
+ln -s $(pwd)/static ../static/casper
+if [ -f "config.json" ]; then
+    cp config.example.json config.json
+    vim config.json
+fi
